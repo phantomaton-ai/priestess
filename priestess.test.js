503e78cf416dcd10a0f838c5fe73b02e935d27aa
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-26 17:39:22 -0700

    Updated by Phantomaton

--- priestess.test.js
+++ priestess.test.js
@@ -21,4 +21,17 @@ describe('Priestess', () => {
     expect(provider.called).to.be.true;
     expect(result).to.equal('Hello, world!');
   });
+
+  it('provides an input symbol', () => {
+    const provider = stub().returns('Input data');
+
+    container.install(priestess.input.resolver());
+    container.install(priestess.input.provider([], () => provider));
+
+    const [input] = container.resolve(priestess.input.resolve);
+    const result = input();
+
+    expect(provider.called).to.be.true;
+    expect(result).to.equal('Input data');
+  });
 });
